05edb48210424a6e0e86013d3e68fd72cecc5efe
zwd93 <276418033@qq.com>
2020-01-30 19:04:45 +0800

    springboot环境测试

---
+++ src/main/java/springboot/controller/TestController.java
@@ -0,0 +1,12 @@
+package springboot.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+	@GetMapping("/hi")
+	public String hi() {
+		return "hi, I'm springboot !";
+	}
+}
